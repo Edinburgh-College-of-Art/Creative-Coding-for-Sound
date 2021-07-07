8bc6933e9bd6b3953ccd3202d8d47aec40b3e10c
mhamilt <matt.hamilton@ed.ac.uk>
2021-07-07 19:42:02 +0100

    condense

--- examples/arduino/ble/imu-ble/imu-ble.ino
+++ examples/arduino/ble/imu-ble/imu-ble.ino
@@ -15,9 +15,8 @@
 //----------------------------------------------------------------------------
 const bool DEBUG = false;
 //----------------------------------------------------------------------------
-float ax = 0, ay = 0, az = 0;
 float accArray[3] = {0};
-bool updateOutput = false;
+float &ax = accArray[0], &ay = accArray[1], &az = accArray[2];
 //----------------------------------------------------------------------------
 BLEService imuService("B1C054FD-E6FC-469F-AA37-E464FF60957D");
 BLECharacteristic imuCharacteristic("B1C054FD-E6FC-469F-AA37-E464FF60957D", BLERead | BLENotify, 12);
@@ -32,25 +31,19 @@ void setup()
   if (!BLE.begin())
     halt("starting BLE failed!");
 
-  if (!IMU.begin())
-  {
-    Serial.println("Failed to initialize IMU!");
-    while (1);
-  }
-
+  if (!IMU.begin())  
+    halt("IMU Failed to start");
+  
   BLE.setLocalName("ArduinoIMU");
   BLE.setAdvertisedService(imuService);
   imuService.addCharacteristic(imuCharacteristic);
   BLE.addService(imuService);
-
   imuCharacteristic.writeValue((void*)accArray, 12); // set the initial value for the characeristic:
 
   BLE.advertise(); // start advertising
 
-  if (DEBUG)
-  {
-    printImuInfo();
-  }
+  if (DEBUG)  
+    printImuInfo();  
 }
 //----------------------------------------------------------------------------
 void loop()
@@ -60,17 +53,14 @@ void loop()
 
   if (IMU.accelerationAvailable())
   {
-    IMU.readAcceleration(ax, ay, az);
-    accArray[0] = ax;
-    accArray[1] = ay;
-    accArray[2] = az;
+    IMU.readAcceleration(ax, ay, az);    
     imuCharacteristic.writeValue((void*)accArray, 12);
   }
 }
 //----------------------------------------------------------------------------
 
 void halt(const char* error)
-{
+{  
   Serial.println(error);
   while (1);
 }
